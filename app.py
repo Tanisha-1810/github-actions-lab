5e6a34f091d9ebeb26ad7a14999f430240c7be75
Ubuntu <azureuser@azureagent.si3qj1t14cwuldxfbxlh0vv1cf.rx.internal.cloudapp.net>
2025-10-28 11:22:35 +0000

    Added sample Python app

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+# This is a test commit
+
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
+    print("All tests passed ✅")
+
+test_add()
