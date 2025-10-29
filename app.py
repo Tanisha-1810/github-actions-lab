2d6d3abff848a28f48a597e510c6da7aac981189
Tanisha-1810 <122026895+Tanisha-1810@users.noreply.github.com>
2025-10-29 16:50:18 +0530

    Refactor app.py by removing tests and adding print
    Removed test function and added print statement.

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 # app.py
-# This is a test commit
 
-def add(a, b):
-    return a + b
+    print("OIDC successfull ")
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
-    print("All tests passed")
-
-test_add()
